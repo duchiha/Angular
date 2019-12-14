30a7ac459306fd3589c45f8cd2968562c30dd5d6
duchiha <duchiha@example.com>
2019-12-14 02:06:19 +0100

    add title to page

--- sample-app/src/app/product-list/product-list.component.ts
+++ sample-app/src/app/product-list/product-list.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  pageTitle : string ='Product Management';
   constructor() { }
 
   ngOnInit() {
