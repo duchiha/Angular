898429c757c3e153feb5a2b9ca542e81a04e5713
duchiha <duchiha@example.com>
2019-12-14 02:06:19 +0100

    adding logic to our view with angular ngif

--- sample-app/src/app/product-list/product-list.component.ts
+++ sample-app/src/app/product-list/product-list.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class ProductListComponent implements OnInit {
 
   pageTitle : string ='Product List';
+  products: any[];
+
   constructor() { }
 
   ngOnInit() {
