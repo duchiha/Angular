963445bc1062474b8cfb6d1b46cf505dab0c657d
duchiha <duchiha@example.com>
2019-12-17 18:19:07 +0100

    add route to product detail

--- sample-app/src/app/components/product-detail/product-detail.component.ts
+++ sample-app/src/app/components/product-detail/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from 'src/app/model/iproduct';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle:string="Product Detail";
+  product:IProduct;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private router:Router) { }
 
   ngOnInit() {
+    let id =+this.route.snapshot.paramMap.get('id');
+    this.pageTitle += ` :${id}`;
+    this.product={
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2019",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    };
+  }
+  onBack():void{
+  this.router.navigate(['/products']);
   }
 
 }
